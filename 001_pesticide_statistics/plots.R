68cd2f6b196cd4a8a13cdca2d9103eb792f1e3f2
Kemal Soylu <kemalbsoylu@gmail.com>
2022-10-15 16:31:49 +0300

    syntax fixes

--- 001_pesticide_statistics/plots.R
+++ 001_pesticide_statistics/plots.R
@@ -7,11 +7,11 @@ library(ggplot2)
 data_1 <- data.frame(read.delim("/data/data_1.csv"))
 
 # Sort data for plots
-data_1$A = with(data_1, reorder(A, B))
+data_1$A <- with(data_1, reorder(A, B))
 
 # Plot 1A
-ggplot(data_1, aes(x=A, y=B, fill=A)) +
-  geom_bar(stat="identity", alpha=.75, width=.5) +
+ggplot(data_1, aes(x = A, y = B, fill = A)) +
+  geom_bar(stat = "identity", alpha = .75, width = .5) +
   scale_y_continuous(
     breaks = seq(0, 4500, by = 500),
   ) +
@@ -25,10 +25,11 @@ ggplot(data_1, aes(x=A, y=B, fill=A)) +
     ) +
   scale_fill_hue(h = c(50, 10)) +
   theme(
-    text = element_text(size=14),
-    axis.title.x = element_text(colour = "grey30", margin = margin(t = 10, unit = "pt")),
+    text = element_text(size = 14),
+    axis.title.x = element_text(colour = "grey30",
+                                margin = margin(t = 10, unit = "pt")),
     panel.grid.major.y = element_blank(),
-    legend.position="none",
+    legend.position = "none",
     plot.title = element_text(size = 14),
     plot.subtitle = element_text(size = 10),
     plot.caption = element_text(size = 8)
@@ -36,8 +37,8 @@ ggplot(data_1, aes(x=A, y=B, fill=A)) +
   coord_flip()
 
 # Plot 1B
-ggplot(data_1, aes(x=A, y=C, fill=A)) +
-  geom_bar(stat="identity", alpha=.75, width=.5) +
+ggplot(data_1, aes(x = A, y = C, fill = A)) +
+  geom_bar(stat = "identity", alpha = .75, width = .5) +
   scale_y_continuous(
     breaks = seq(0, 15, by = 2),
   ) +
@@ -51,10 +52,11 @@ ggplot(data_1, aes(x=A, y=C, fill=A)) +
   ) +
   scale_fill_hue(h = c(200, 240)) +
   theme(
-    text = element_text(size=14),
-    axis.title.x = element_text(colour = "grey30", margin = margin(t = 10, unit = "pt")),
+    text = element_text(size = 14),
+    axis.title.x = element_text(colour = "grey30",
+                                margin = margin(t = 10, unit = "pt")),
     panel.grid.major.y = element_blank(),
-    legend.position="none",
+    legend.position = "none",
     plot.title = element_text(size = 14),
     plot.subtitle = element_text(size = 10),
     plot.caption = element_text(size = 8)
@@ -65,11 +67,11 @@ ggplot(data_1, aes(x=A, y=C, fill=A)) +
 data_2 <- data.frame(read.delim("/data/data_2.csv"))
 
 # Sort data for plots
-data_2$A = with(data_2, reorder(A, B))
+data_2$A <- with(data_2, reorder(A, B))
 
 # Plot 2A
-ggplot(data_2, aes(x=A, y=B, fill=A)) +
-  geom_bar(stat="identity", alpha=.75, width=.5) +
+ggplot(data_2, aes(x = A, y = B, fill = A)) +
+  geom_bar(stat = "identity", alpha = .75, width = .5) +
   scale_y_continuous(
     breaks = seq(0, 20000, by = 3000),
   ) +
@@ -82,10 +84,11 @@ ggplot(data_2, aes(x=A, y=B, fill=A)) +
     caption = "*Diğerleri: Bitki Aktivatörü+Bitki Gelişim Düzenleyici+Böcek Cezbedici+Fumigant+Nematisit, Kaynak: T.C. Tarım Ve Orman Bakanlığı Gıda Ve Kontrol Genel Müdürlüğü"
   ) +
   theme(
-    text = element_text(size=14),
-    axis.title.x = element_text(colour = "grey30", margin = margin(t = 10, unit = "pt")),
+    text = element_text(size = 14),
+    axis.title.x = element_text(colour = "grey30",
+                                margin = margin(t = 10, unit = "pt")),
     panel.grid.major.y = element_blank(),
-    legend.position="none",
+    legend.position = "none",
     plot.title = element_text(size = 14),
     plot.subtitle = element_text(size = 10),
     plot.caption = element_text(size = 8)
@@ -93,8 +96,8 @@ ggplot(data_2, aes(x=A, y=B, fill=A)) +
   coord_flip()
 
 # Plot 2B
-ggplot(data_2, aes(x=A, y=C, fill=A)) +
-  geom_bar(stat="identity", alpha=.75, width=.5) +
+ggplot(data_2, aes(x = A, y = C, fill = A)) +
+  geom_bar(stat = "identity", alpha = .75, width = .5) +
   scale_y_continuous(
     breaks = seq(0, 40, by = 4),
   ) +
@@ -107,10 +110,11 @@ ggplot(data_2, aes(x=A, y=C, fill=A)) +
     caption = "*Diğerleri: Bitki Aktivatörü+Bitki Gelişim Düzenleyici+Böcek Cezbedici+Fumigant+Nematisit, Kaynak: T.C. Tarım Ve Orman Bakanlığı Gıda Ve Kontrol Genel Müdürlüğü"
   ) +
   theme(
-    text = element_text(size=14),
-    axis.title.x = element_text(colour = "grey30", margin = margin(t = 10, unit = "pt")),
+    text = element_text(size = 14),
+    axis.title.x = element_text(colour = "grey30",
+                                margin = margin(t = 10, unit = "pt")),
     panel.grid.major.y = element_blank(),
-    legend.position="none",
+    legend.position = "none",
     plot.title = element_text(size = 14),
     plot.subtitle = element_text(size = 10),
     plot.caption = element_text(size = 8)
